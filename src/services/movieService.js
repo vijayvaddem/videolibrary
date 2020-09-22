9ed148f5774c8a238971feedafc81d208e87d5a2
Vijay Vaddem <vijay.vaddem@gmail.com>
2020-09-22 10:06:25 +0530

    cleared keychain access...retying again

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -15,6 +15,7 @@ export function saveMovie(movie) {
   movieInDb.numberInStock = movie.numberInStock;
   movieInDb.dailyRentalRate = movie.dailyRentalRate;
 
+  
   if (!movieInDb._id) {
     movieInDb._id = Date.now().toString();
     movies.push(movieInDb);
